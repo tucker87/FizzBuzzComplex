b5c7905b03de06778b11d80c1c33a681f76d2508
Jason Tucker <jtgeek@gmail.com>
2020-10-28 16:21:23 -0400

    Makes results IEnumerable with yield

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -13,7 +13,6 @@ namespace FizzBuzzComplex
             Formula2Word = formula2Word;
             Formula1 = i => i % divisor1 == 0;
             Formula2 = i => i % divisor2 == 0;
-            Results = BuildList();
         }
 
         public FizzBuzz(Func<int, bool> formula1, Func<int, bool> formula2, int start = 1, int end = 100, string formula1Word = "Fizz", string formula2Word = "Buzz") 
@@ -24,7 +23,6 @@ namespace FizzBuzzComplex
             Formula2Word = formula2Word;
             Formula1 = formula1;
             Formula2 = formula2;
-            Results = BuildList();
         }
 
         public int Start { get; private set; }
@@ -33,15 +31,13 @@ namespace FizzBuzzComplex
         public static Func<int, bool> Formula2 { get; private set; }
         public string Formula1Word { get; private set; }
         public string Formula2Word { get; private set; }
-        public List<string> Results { get; private set; }
-
-        private List<string> BuildList()
+        public IEnumerable<string> Results
         {
-            var results = new List<string>();
-            for (var i = Start; i <= End; i++)
-                results.Add(FizzOrBuzz(i));
-
-            return results;
+            get
+            {
+                for (var i = Start; i <= End; i++)
+                    yield return FizzOrBuzz(i);
+            }
         }
 
         public string FizzOrBuzz(int i)
